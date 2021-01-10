b168df7582b938c7761d203c0921f3d362d3dcc2
Tyrell-CircuitStream <tyrell.lewin@gmail.com>
2021-01-10 14:49:21 -0800

    Teleportation Added

---
+++ Assets/Scripts/TeleportationZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationZone : MonoBehaviour
+{
+    public Transform snapRef;
+    
+    public Vector3 GetSnapPos()
+    {
+        return snapRef.position;
+    }
+}
--- Assets/Scripts/VRHands.cs
+++ Assets/Scripts/VRHands.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -17,6 +18,18 @@ public class VRHands : MonoBehaviour
     public Transform XRRig;
     public LineRenderer lR;
 
+    public float curverHeight = 2;
+    public int positionCount = 10;
+
+    public Transform teleportEffect;
+    public float smoothnessValue = 10;
+
+    public RawImage fadeScreen;
+    public float fadeDuration = 0.8f;
+    public float waitTime = 0.5f;
+
+    private bool teleportLock = false;
+
     public struct SumInfo
     {
         public bool isGreaterThan5;
@@ -26,7 +39,7 @@ public class VRHands : MonoBehaviour
 
     public void Start()
     {
-        lR.positionCount = 2;
+        lR.positionCount = positionCount;
         lR.enabled = false;
     }
 
@@ -49,17 +62,113 @@ public class VRHands : MonoBehaviour
         {
             lR.enabled = true;
 
-            lR.SetPosition(0, transform.position);
-            lR.SetPosition(1, hitInfo.point);
+            Vector3 startPoint = transform.position;
 
-            if (Input.GetButtonDown(handness.ToString() + "TriggerPress"))
+            TeleportationZone teleZone = hitInfo.collider.GetComponent<TeleportationZone>();
+
+            Vector3 desiredPoint;
+            if (teleZone != null)
             {
-                XRRig.position = hitInfo.point;
+                desiredPoint = teleZone.GetSnapPos();
+            }
+            else
+            {
+                desiredPoint = hitInfo.point;
+            }
+
+
+            Vector3 vecToDesired = desiredPoint - teleportEffect.position;
+            vecToDesired /= smoothnessValue;
+            vecToDesired *= Time.deltaTime;
+            Vector3 endPoint = vecToDesired + teleportEffect.position;
+
+
+            teleportEffect.gameObject.SetActive(true);
+            teleportEffect.position = endPoint;
+
+
+            Vector3 midPoint = ((endPoint - startPoint) / 2f) + startPoint;
+            midPoint.y += curverHeight;
+
+            for (int i = 0; i < positionCount; i = i + 1)
+            {
+                Vector3 lerp1 = Vector3.Lerp(startPoint, midPoint, i / (float)positionCount);
+                Vector3 lerp2 = Vector3.Lerp(midPoint, endPoint, i / (float)positionCount);
+                Vector3 curvedPos = Vector3.Lerp(lerp1, lerp2, i / (float)positionCount);
+
+                lR.SetPosition(i, curvedPos);
+            }
+
+            if (Input.GetButtonDown(handness.ToString() + "TriggerPress") && teleportLock == false)
+            {
+                StartCoroutine(FadeTeleport(endPoint));
             }
         }
         else
         {
             lR.enabled = false;
+            teleportEffect.gameObject.SetActive(false);
         }
+    }
+
+    public IEnumerator FadeTeleport(Vector3 posToTeleTo)
+    {
+        teleportLock = true;
+
+        // Initialize our state
+        float currentTime = 0;
+        fadeScreen.enabled = true;
+        Color black = Color.black;
+        Color clear = Color.clear;
+        fadeScreen.color = clear;
+
+        // Start our loop, and loop until time is up
+        while(currentTime < 1)
+        {
+            // Fade the screen i.e. start making the screen black
+            fadeScreen.color = Color.Lerp(clear, black, currentTime);
+
+            // Wait one frame
+            yield return null;
+
+            // Increase Current time
+            currentTime += Time.deltaTime / fadeDuration;
+
+            // Loop again
+        }
+
+        // Make sure screen is fully black
+        fadeScreen.color = black;
+
+        // Wait just a little bit and teleport
+        yield return new WaitForSeconds(waitTime);
+        XRRig.position = posToTeleTo;
+
+        // Then start fading back in
+
+        // Initialize our state
+        currentTime = 0;
+
+        // Start our loop, and loop until time is up
+        while (currentTime < 1)
+        {
+            // Fade the screen i.e. start making the screen black
+            fadeScreen.color = Color.Lerp(black, clear, currentTime);
+
+            // Wait one frame
+            yield return null;
+
+            // Increase Current time
+            currentTime += Time.deltaTime / fadeDuration;
+
+            // Loop again
+        }
+
+        // Make sure screen is fully black
+        fadeScreen.color = clear;
+        fadeScreen.enabled = false;
+
+        teleportLock = false;
+
     }
 }
